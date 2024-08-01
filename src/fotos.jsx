e9bdb9cbaa6b1bb07959160fa53c8185614020fc
Mariano <Marianofogel17@gmail.com>
2024-07-31 23:46:55 -0300

    Que no tire error fotos

--- src/fotos.jsx
+++ src/fotos.jsx
@@ -3,13 +3,11 @@ import fotoMati from './assets/img/mati.png'
 
 export function Perfil() {
     
-    <div>
-    <img src={fotoMarian} alt="Foto chiffo" />
-    </div>
-
-    <div>
-    <img src={fotoMati} alt="Foto chiffo" />
-    </div>
+    <><div>
+        <img src={fotoMarian} alt="Foto chiffo" />
+    </div><div>
+            <img src={fotoMati} alt="Foto chiffo" />
+        </div></>
 
 
 
